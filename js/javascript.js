fbbac77f592f43466bdf8f4916886e579ba059fc
Alexdev8 <alexisbensoussan06@gmail.com>
2022-12-14 14:45:40 +0100

    suuuuuuuuuuu

--- js/javascript.js
+++ js/javascript.js
@@ -1,8 +1,9 @@
-let input = document.getElementById("test");
+let input = document.getElementById("input_name");
+let skier = document.getElementById("skier");
 
 window.addEventListener("keydown", function(event) {
     let str;
-    switch(event.code){
+    switch(event.key){
         case "ArrowDown":
             str = "Flèche bas"
             break;
@@ -11,18 +12,28 @@ window.addEventListener("keydown", function(event) {
             break;
         case "ArrowLeft":
             str = "Flèche gauche"
+            move_left();
             break;
         case "ArrowRight":
             str = "Flèche droite"
+            move_right();
             break;
+        default:
+            str= event.key
     }
     input.textContent = str
 }, true);
 
+window.addEventListener("keyup", function(event) {
+    if (event.key == "ArrowRight" || event.key == "ArrowLeft"){
+        skier.src = "../images/skier.png";
+    }
+}, true);
+
 function move_right(){
-    
+    skier.src = "../images/skier_right.png";
 }
 
 function move_left(){
-    
+    skier.src = "../images/skier_left.png";
 }
