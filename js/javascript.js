9a5f2b999d7d418f17ce2d52ca6709689492cfd3
Alexdev8 <alexisbensoussan06@gmail.com>
2022-12-21 18:40:56 +0100

    changement de direction du skieur avec fleches directionnelles : fonctionnel

--- js/javascript.js
+++ js/javascript.js
@@ -1,32 +1,44 @@
 let input = document.getElementById("input_name");
 let skier = document.getElementById("skier");
 
+let pressed_key = "";
+let timeout = null;
+
 window.addEventListener("keydown", function(event) {
     let str;
     switch(event.key){
         case "ArrowDown":
-            str = "Flèche bas"
+            str = "Flèche bas";
             break;
         case "ArrowUp":
-            str = "Flèche haut"
+            str = "Flèche haut";
             break;
         case "ArrowLeft":
-            str = "Flèche gauche"
+            str = "Flèche gauche";
+            pressed_key = "ArrowLeft";
+            if (timeout !== null){
+                this.clearTimeout(timeout);
+            }
             move_left();
             break;
         case "ArrowRight":
-            str = "Flèche droite"
+            str = "Flèche droite";
+            pressed_key = "ArrowRight";
+            if (timeout !== null){
+                this.clearTimeout(timeout);
+            }
             move_right();
             break;
         default:
-            str= event.key
+            str = event.key;
     }
     input.textContent = str
 }, true);
 
 window.addEventListener("keyup", function(event) {
-    this.setTimeout(function(){
-        if (event.key == "ArrowRight" || event.key == "ArrowLeft"){
+    pressed_key = "";
+    timeout = this.setTimeout(function(){
+        if ((event.key == "ArrowRight" || event.key == "ArrowLeft") && pressed_key == ""){
             skier.src = "../images/skier.png";
         }
     }, 300)
